a6141db7a4472d555972155d693f7fb84adea70c
Yuliana Mallorga <yulianamallorga@gmail.com>
2024-11-21 16:21:23 -0300

    Restore HTTP POST path for /signin on Node.js server and implement user authentication with bcrypt password validation

--- server.cjs
+++ server.cjs
@@ -24,7 +24,23 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors()); 
 
-
+app.post('/signin', (req, res) => {
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })    
+      } else {
+        res.status(401).json('invalid user or credentials')
+      }
+    })
+    .catch(err => res.status(401).json('invalid user or credentials'))
+})
 
 app.post('/register', (req, res) =>{
   const { email, name, password } = req.body; 
