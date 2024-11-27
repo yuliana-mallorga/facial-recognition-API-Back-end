59103dcd479835fbf083ffd37f002b49246fb46d
Yuliana Mallorga <yulianamallorga@gmail.com>
2024-11-27 00:06:58 -0300

    Add profile controller for retrieving user data.

--- server.cjs
+++ server.cjs
@@ -10,6 +10,8 @@ const { handleRegister } = require('./controllers/register.cjs');
 
 const { handleSignin } = require('./controllers/signin.cjs');
 
+const { handleProfileGet } = require('./controllers/profile.cjs');
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -28,24 +30,11 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors()); 
 
-app.post('/signin', (req, res) =>{ handleSignin(req, res, db, bcrypt)})
+app.post('/signin', (req, res) =>{ handleSignin(req, res, db, bcrypt) })
 
-app.post('/register', (req, res) => {handleRegister(req, res, db, bcrypt)})
+app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) })
 
-app.get('/profile/:id', (req, res) =>{
-  const { id } = req.params;
-  db.select('*').from('users').where({id})
-  .then(user => {
-    if(user.length) {
-      res.status(200).json(user[0]);
-    } else {
-      res.status(404).json('Not found');
-    }
-  })
-  .catch(err =>{
-    res.status(400).json('Error getting user');
-  })
-})
+app.get('/profile/:id', (req, res) => { handleProfileGet(req, res, db) })
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
