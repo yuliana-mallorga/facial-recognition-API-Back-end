f2501d513c5b4d87da5095b8c105c19ae5052846
Yuliana Mallorga <yulianamallorga@gmail.com>
2024-12-10 17:24:00 -0300

    Add database conection.

--- server.cjs
+++ server.cjs
@@ -13,10 +13,8 @@ const { handleApiCall } = require('./controllers/image.cjs');
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.DATABASE_URL,
+    ssl: true 
   },
 });
 
