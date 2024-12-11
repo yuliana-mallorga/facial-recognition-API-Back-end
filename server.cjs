55367eb730575ba11b36731c93b54b87cb706173
Yuliana Mallorga <yulianamallorga@gmail.com>
2024-12-11 16:24:19 -0300

    Refactor database connection logic.

--- server.cjs
+++ server.cjs
@@ -13,8 +13,13 @@ const { handleApiCall } = require('./controllers/image.cjs');
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
-    ssl: true 
+    connectionString: process.env.VITE_DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    host: process.env.DB_HOST,
+    port:5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DB
   },
 });
 
